f109903408f1dc790b0b7bc2eb2d289b53f63b3d
tanveermedaveer <tanveermedaveer@example.com>
2026-08-13 12:21:16 +0500

    Add error capture wrapper in api/index.cjs

--- api/index.cjs
+++ api/index.cjs
@@ -1,3 +1,15 @@
-const app = require('../server/server.js');
-
+let app;
+try {
+  app = require('../server/server.js');
+} catch (err) {
+  const express = require('express');
+  app = express();
+  app.all('*', (req, res) => {
+    res.status(500).json({
+      error: 'Module Load Failed',
+      message: err.message,
+      stack: err.stack
+    });
+  });
+}
 module.exports = app;
